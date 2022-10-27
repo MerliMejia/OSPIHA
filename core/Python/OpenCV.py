01d3e7a330dfd9cd6384c02644c7f09f5be6992a
Merli Mejia <merlimejia25@gmail.com>
2022-10-27 19:49:06 -0400

    [OSPIHA-14] Add island images (edge created).

--- core/Python/OpenCV.py
+++ core/Python/OpenCV.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-img = cv2.imread("./utils/testimg.png")
+img = cv2.imread("./test-files/islands/Smugglers' Bay.png")
 # Convert to graycsale
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # Blur the image for better edge detection
@@ -9,4 +9,4 @@ img_blur = cv2.GaussianBlur(img_gray, (3,3), 0)
 
 edges = cv2.Canny(image=img_blur, threshold1=50, threshold2=50) # Canny Edge Detection
 
-cv2.imwrite("./utils/testimg.png", edges)
+cv2.imwrite("./test-files/islands/Smugglers' Bay.png", edges)
