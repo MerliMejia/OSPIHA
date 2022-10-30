43e0c236f6ba5494c058da90a0ab8ff838b3c76e
Merli Mejia <merlimejia25@gmail.com>
2022-10-30 17:54:05 -0400

    [OSPIHA-18] Get Similarity Endpoint Test.

--- core/Python/OpenCV.py
+++ core/Python/OpenCV.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-img = cv2.imread("./test-files/islands/Smugglers' Bay.png")
+img = cv2.imread("./test-files/islands/Snake Island.png")
 # Convert to graycsale
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # Blur the image for better edge detection
@@ -9,4 +9,4 @@ img_blur = cv2.GaussianBlur(img_gray, (3,3), 0)
 
 edges = cv2.Canny(image=img_blur, threshold1=50, threshold2=50) # Canny Edge Detection
 
-cv2.imwrite("./test-files/islands/Smugglers' Bay.png", edges)
+cv2.imwrite("./test-files/islands/Snake Island.png", edges)
--- pages/api/HTTP-Test.test.ts
+++ pages/api/HTTP-Test.test.ts
@@ -20,7 +20,7 @@ describe('HTTP Test', () => {
   describe('Get Hash Endpoint', () => {
     it('Should send a file and return the hash string', (done) => {
       let data: FormData = formData();
-      let file = fs.readFileSync(path.resolve('./test-files/cat.png'));
+      let file = fs.readFileSync(path.resolve('./test-files/snake-island-test.png'));
       data.append('file', file.toString());
       post(BASE_URL + 'hash', { body: data })
         .then((res) => {
@@ -62,6 +62,75 @@ describe('HTTP Test', () => {
       let file = fs.readFileSync(path.resolve('./test-files/chicken.png'));
       data.append('file', file.toString());
       post(BASE_URL + 'hash', { body: data })
+        .then((res) => {
+          expect(res.status).toBe(400);
+          done();
+        })
+        .catch((err) => {
+          done(err);
+        });
+    });
+  });
+  describe('Get Similarity Endpoint', () => {
+    it('Should send 2 images and return the similarity % greather than 90%', (done) => {
+      let data: FormData = formData();
+      let file1 = fs.readFileSync(path.resolve('./test-files/snake-island-test.png'));
+      let file2 = fs.readFileSync(path.resolve('./test-files/islands/Snake Island.png'));
+      data.append('file1', file1.toString());
+      data.append('file2', file2.toString());
+      post(BASE_URL + 'similarity', { body: data })
+        .then((res) => {
+          expect(res.status).toBe(200);
+          expect(res.json).toHaveProperty('data');
+          expect(res.json.data.similarity).toBeGreaterThan(90);
+          done();
+        })
+        .catch((err) => {
+          done(err);
+        });
+    });
+    it('Should not send a file and return an error', (done) => {
+      let data: FormData = formData();
+      post(BASE_URL + 'similarity', { body: data })
+        .then((res) => {
+          expect(res.status).toBe(400);
+          done();
+        })
+        .catch((err) => {
+          done(err);
+        });
+    });
+    it('Should send only one file and return an error', (done) => {
+      let data: FormData = formData();
+      let file1 = fs.readFileSync(path.resolve('./test-files/snake-island-test.png'));
+      data.append("file1", file1.toString())
+      post(BASE_URL + 'similarity', { body: data })
+        .then((res) => {
+          expect(res.status).toBe(400);
+          done();
+        })
+        .catch((err) => {
+          done(err);
+        });
+    });
+    it('Should send a file that is not an image and return an error', (done) => {
+      let data: FormData = formData();
+      let file = fs.readFileSync(path.resolve('./test-files/test.txt'));
+      data.append('file1', file.toString());
+      post(BASE_URL + 'similarity', { body: data })
+        .then((res) => {
+          expect(res.status).toBe(400);
+          done();
+        })
+        .catch((err) => {
+          done(err);
+        });
+    });
+    it('Should send a file that is larger than 10MG and return an error', (done) => {
+      let data: FormData = formData();
+      let file = fs.readFileSync(path.resolve('./test-files/chicken.png'));
+      data.append('file1', file.toString());
+      post(BASE_URL + 'similarity', { body: data })
         .then((res) => {
           expect(res.status).toBe(400);
           done();
