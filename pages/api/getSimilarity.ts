aa0e7d1a9895833427edae8ac53cc3597c3c3ca8
Merli Mejia <merlimejia25@gmail.com>
2022-11-30 13:11:40 -0400

    getSimilarity endpoint.

--- pages/api/getSimilarity.ts
+++ pages/api/getSimilarity.ts
@@ -1,4 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { CalculateSimilarity } from '../../core/PerceptualHashing';
+import {
+  GetFilePathFromClientUpload,
+  MakeCannyEdgeDetection
+} from '../../utils/helpers';
 import { ApiResponse } from '../../utils/interfaces';
 
 type IResponse = ApiResponse<{ similarity: number }>;
@@ -12,4 +17,20 @@ export const config = {
 export default async function post(
   req: NextApiRequest,
   res: NextApiResponse<IResponse>
-) {}
+) {
+  //Getting file paths
+  const filePaths: string[] = (await GetFilePathFromClientUpload(
+    req,
+    false
+  )) as string[];
+
+  filePaths.forEach(async (filePath) => {
+    //Converting image to lines only.
+    await MakeCannyEdgeDetection(filePath);
+  });
+
+  //Calculate similarity based on Hamming Distance.
+  const similarity = await CalculateSimilarity(filePaths[0], filePaths[1]);
+
+  res.send({ data: { similarity } });
+}
